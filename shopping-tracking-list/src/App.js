630069cf92cc97bf9dece58bc849defb2efd12b7
royc67 <royc67@gmail.com>
2021-03-13 15:21:08 +0200

    Update App.js

--- shopping-tracking-list/src/App.js
+++ shopping-tracking-list/src/App.js
@@ -1,5 +1,35 @@
+import Navbar from "./components/Navbar";
+import { useReducer } from "react";
+
+const ACTIONS = {
+  ADD_ITEM: "addItem",
+  ARCHIVE_ITEM: "archive",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_ITEM:
+      break;
+
+    case ACTIONS.ARCHIVE:
+      break;
+
+    default:
+      return { ...state };
+  }
+}
+
 function App() {
-  return <div>hi</div>;
+  const [state, dispatch] = useReducer(reducer, {
+    deliveryItems: [],
+    archivedItems: [],
+  });
+
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
