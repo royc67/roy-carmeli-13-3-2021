4afe989e095a78a3ab2b5f9161e671d1e6ba7066
royc67 <royc67@gmail.com>
2021-03-14 16:44:39 +0200

    Create PageNavbar.jsx

---
+++ shopping-tracking-list/src/components/PageNavbar.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import { Tabs, Tab, AppBar, Toolbar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+function a11yProps(index) {
+  return {
+    id: `standard-tab-${index}`,
+    "aria-controls": `standard-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 500,
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+export default function PageNavbar() {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <AppBar className={classes.appBar} position="static" color="default">
+      <Toolbar>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="standard"
+          aria-label="tabs"
+        >
+          <Tab
+            label="delivery"
+            {...a11yProps(0)}
+            component={Link}
+            to="/purchase/byItem/delivery"
+          />
+          <Tab
+            label="archive"
+            {...a11yProps(1)}
+            component={Link}
+            to="/purchase/byItem/archive"
+          />
+        </Tabs>
+      </Toolbar>
+    </AppBar>
+  );
+}
