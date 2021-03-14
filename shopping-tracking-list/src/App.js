83d16a7ecfd86e12bc1a89c6fb5211aeea88999a
royc67 <royc67@gmail.com>
2021-03-14 16:44:19 +0200

    Update App.js

--- shopping-tracking-list/src/App.js
+++ shopping-tracking-list/src/App.js
@@ -1,12 +1,22 @@
-import Navbar from "./components/Navbar";
-import useApp from "./Hooks/useApp";
+import Navbar from "./components/Navbar.jsx";
+// import useApp from "./Hooks/useApp";
 import { AppProvider } from "./contexts/AppContext";
+import PurchaseByItem from "./pages/PurchaseByItem";
+import PurchaseByStore from "./pages/PurchaseByStore";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <AppProvider></AppProvider>
+      <AppProvider>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/purchase/byItem" component={PurchaseByItem} />
+            <Route exact path="/purchase/byStore" component={PurchaseByStore} />
+          </Switch>
+        </Router>
+      </AppProvider>
     </div>
   );
 }
