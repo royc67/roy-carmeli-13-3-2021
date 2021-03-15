9a3d087fae711b55d2d73809a32d6acb5051b301
royc67 <royc67@gmail.com>
2021-03-15 23:34:57 +0200

    Update ArchiveItems.jsx

--- shopping-tracking-list/src/components/ArchiveItems.jsx
+++ shopping-tracking-list/src/components/ArchiveItems.jsx
@@ -24,6 +24,9 @@ const useStyles = makeStyles((theme) => ({
   dark: {
     backgroundColor: "#eaeaea",
   },
+  toolBar: {
+    margin: theme.spacing(2),
+  },
   search: {
     position: "relative",
     borderRadius: theme.shape.borderRadius,
@@ -84,6 +87,7 @@ export default function ArchiveItems() {
         alignItems="center"
       >
         <Grid
+          className={classes.toolBar}
           container
           direction="row"
           justify="space-between"
