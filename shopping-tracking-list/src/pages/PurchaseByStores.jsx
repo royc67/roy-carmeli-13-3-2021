2435f65fecd98787aa94b79f4ac310a7efd654e7
royc67 <royc67@gmail.com>
2021-03-15 09:27:54 +0200

    Update PurchaseByStores.jsx

--- shopping-tracking-list/src/pages/PurchaseByStores.jsx
+++ shopping-tracking-list/src/pages/PurchaseByStores.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { fade, makeStyles } from "@material-ui/core/styles";
-import { Grid, Button, InputBase } from "@material-ui/core";
-import AddIcon from "@material-ui/icons/Add";
+import { Grid, InputBase } from "@material-ui/core";
+import useApp from "../Hooks/useApp";
 import SearchIcon from "@material-ui/icons/Search";
 import {
   TableHead,
@@ -13,28 +13,6 @@ import {
   TableBody,
 } from "@material-ui/core";
 
-function createItem(name, store, price, dest) {
-  return { name, store, price, dest };
-}
-
-const rows = [
-  createItem("Frozen yoghurt", "159", "6.0", "24"),
-  createItem("Ice cream sandwich", 237, 9.0, 37),
-  createItem("Eclair", 262, 16.0, 24),
-  createItem("Cupcake", 305, 3.7, 67),
-  createItem("Gingerbread", 356, 16.0, 49),
-  createItem("Frozen yoghurt", "159", "6.0", "24"),
-  createItem("Ice cream sandwich", 237, 9.0, 37),
-  createItem("Eclair", 262, 16.0, 24),
-  createItem("Cupcake", 305, 3.7, 67),
-  createItem("Ice cream sandwich", 237, 9.0, 37),
-  createItem("Eclair", 262, 16.0, 24),
-  createItem("Cupcake", 305, 3.7, 67),
-  createItem("Ice cream sandwich", 237, 9.0, 37),
-  createItem("Eclair", 262, 16.0, 24),
-  createItem("Cupcake", 305, 3.7, 67),
-  createItem("Ice cream sandwich", 237, 9.0, 37),
-];
 const useStyles = makeStyles((theme) => ({
   table: {
     minWidth: "100vmin",
@@ -92,10 +70,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function PurchaseByStores() {
   const classes = useStyles();
-
-  function archive(item) {
-    //   archive item
-  }
+  const [state, dispach] = useApp();
 
   return (
     <Grid container>
@@ -135,7 +110,7 @@ export default function PurchaseByStores() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row, i) => (
+              {state.archiveItems.map((row, i) => (
                 <TableRow key={row.name} className={i % 2 && classes.dark}>
                   <TableCell component="th" scope="row">
                     {row.name}
