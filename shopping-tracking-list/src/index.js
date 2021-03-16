896c5b26581e450389743c342944839f65803f76
royc67 <royc67@gmail.com>
2021-03-16 18:11:04 +0200

    disable strictMode, moved appProvider to index.js

--- shopping-tracking-list/src/index.js
+++ shopping-tracking-list/src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { AppProvider } from "./contexts/AppContext";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <AppProvider>
     <App />
-  </React.StrictMode>,
+  </AppProvider>,
+  // </React.StrictMode>
   document.getElementById("root")
 );
 
