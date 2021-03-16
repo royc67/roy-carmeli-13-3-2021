f4e620a668171e268150d7d9ea3d4775ac8338bf
royc67 <royc67@gmail.com>
2021-03-16 11:49:58 +0200

    add styles

--- shopping-tracking-list/src/App.js
+++ shopping-tracking-list/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar.jsx";
 import { AppProvider } from "./contexts/AppContext";
+import { makeStyles } from "@material-ui/core/styles";
 import PurchaseByItem from "./pages/PurchaseByItem";
 import PurchaseByStores from "./pages/PurchaseByStores";
 import { Grid } from "@material-ui/core";
@@ -10,9 +11,15 @@ import {
   Redirect,
 } from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <Grid>
+    <Grid className={classes.root}>
       <AppProvider>
         <Router>
           <Redirect to="/purchase/byItem/delivery" />
