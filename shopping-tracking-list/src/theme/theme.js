6263bdeadfd76b9013aa1bf32db4697b87920465
royc67 <royc67@gmail.com>
2021-03-16 20:51:00 +0200

    Create theme.js

---
+++ shopping-tracking-list/src/theme/theme.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+  palette: {
+    secondary: {
+      light: "#8db4e0",
+      main: "#661fd1",
+      dark: "rgba(91, 97, 251, 0.6)",
+    },
+    primary: {
+      light: "#00e3ce",
+      main: "#75c9c9",
+      dark: "rgba(117, 201, 201, 0.6)",
+    },
+  },
+});
