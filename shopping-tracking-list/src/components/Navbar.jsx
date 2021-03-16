17bb4e3cab20beb737f366a3efb2317123a4c067
royc67 <royc67@gmail.com>
2021-03-16 18:10:14 +0200

    currency api error message

--- shopping-tracking-list/src/components/Navbar.jsx
+++ shopping-tracking-list/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Tab, Tabs, AppBar, Toolbar } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import useApp from "../Hooks/useApp";
+
 function a11yProps(index) {
   return {
     id: `standard-tab-${index}`,
@@ -12,6 +13,7 @@ function a11yProps(index) {
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
+    width: "100%",
     display: "flex",
     transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
@@ -30,6 +32,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const API_ERROR_MESSAGE =
+  "Currency rates API is no accessible, trying to re-connect...";
+
 export default function Navbar() {
   const classes = useStyles();
   const [tab, setTab] = useState(0);
@@ -44,11 +49,14 @@ export default function Navbar() {
     const fetchCurrencyRate = () => {
       fetch("https://api.exchangeratesapi.io/latest?base=USD")
         .then((res) => res.json())
-        .then((json) =>
+        .then((json) => {
           dispatch({
             type: "updateConverter",
             payload: { newValue: json.rates.ILS },
-          })
+          });
+        })
+        .catch((err) =>
+          dispatch({ type: "error", payload: [true, API_ERROR_MESSAGE] })
         );
     };
 
